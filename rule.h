9b21e17dd4334a56002b770be851a779e9d9bd81
Krovavic666 <wolfghost86@gmail.com>
2017-07-18 13:05:36 +0300

    carcas

---
+++ rule.h
@@ -0,0 +1,28 @@
+/*
+ * rule.h
+ *
+ *  Created on: 18.07.2017
+ *      Author: student
+ */
+
+#define RULE_H_
+using namespace std;
+#include <iostream.h>
+class rule {
+public:
+	virtual void apply(bool,int)=0;
+};
+
+class Rule5: public rule {
+public:
+	virtual void apply (bool,int);
+};
+
+class rulelife: public rule {
+public:
+	virtual void apply(bool,int);
+};
+
+
+
+
