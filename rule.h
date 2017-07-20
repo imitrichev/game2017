ec694376bcded94e920aeafbf850855c748c0a30
Ivan Mitrichev <imitrichev@users.noreply.github.com>
2017-07-21 02:39:53 +0300

    Update rule.h
    Capitalize. Remove trailing newlines

--- rule.h
+++ rule.h
@@ -1,5 +1,5 @@
 /*
- * rule.h
+ * Rule.h
  *
  *  Created on: 18.07.2017
  *      Author: student
@@ -8,21 +8,17 @@
 #define RULE_H_
 using namespace std;
 #include <iostream.h>
-class rule {
+class Rule {
 public:
 	virtual void apply(bool,int)=0;
 };
 
-class Rule5: public rule {
+class Rule5: public Rule {
 public:
 	virtual void apply (bool,int);
 };
 
-class RuleLife: public rule {
+class RuleLife: public Rule {
 public:
 	virtual void apply(bool,int);
 };
-
-
-
-
